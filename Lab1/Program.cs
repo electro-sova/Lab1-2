1ae35f62fa7b71674193808911616d8029d2e02f
electro-sova <timurpolina28@gmail.com>
2023-12-11 20:26:58 +0500

    first commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace Lab1
+{
+    public class Program
+    {
+        public static int[] Sort(int[] array)
+        {
+            static void Swap(ref int a, ref int b)
+            {
+                var c = a;
+                a = b;
+                b = c;
+            }
+
+            var length = array.Length;
+
+            for (var i = 1; i < length; i++)
+            {
+                for (var j = 0; j < length - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+
+            return array;
+        }
+        public static bool Palindrome(string s)
+        {
+            if (s.Length < 2)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number < 0) throw new Exception("Число меньше нуля");
+
+            int fact = 1;
+
+            for (int i = 1; i <= number; i++) fact *= i;
+
+            return fact;
+        }
+        public static int Fibonacci(int number)
+        {
+            if (number < 0) throw new Exception("Число меньше нуля");
+
+            if (number == 1) return 1;
+            else if (number == 0) return 0;
+
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+
+        public static double Stepen(double a, double b)
+        {
+            if (a <= 0) throw new Exception("Число меньше или равняется нулю");
+
+            return Math.Pow(a, b);
+        }
+        public static bool PrimeNumber(int number)
+        {
+            if (number < 0) return false;
+
+            for (int i = 2; i < Math.Sqrt(number) + 1; i++) if (number % i == 0) return false;
+
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] a = { 10, 9, 11, 225, 224, 956, 304, 2, 5 };
+
+            Console.WriteLine("Отсортированный массив:");
+            foreach (var e in Sort(a)) Console.Write(e + " ");
+
+            Console.WriteLine("Числа факториала");
+            Console.WriteLine($"7! = {Factorial(5)}"); //5040
+            Console.WriteLine($"4! = {Factorial(6)}"); //24
+
+            Console.WriteLine("\nСлова палиндромы");
+
+            string s1 = "kkkkjjjj";
+            string s2 = "112211";
+
+            Console.WriteLine($"{s1} = {Palindrome(s1)}"); //false
+            Console.WriteLine($"{s2} = {Palindrome(s2)}"); //true
+
+            Console.WriteLine("Числа фибоначчи");
+            Console.WriteLine($"5 = {Fibonacci(11)}"); // 89
+            Console.WriteLine($"6 = {Fibonacci(25)}"); // 75025
+
+            Console.WriteLine("Возведение в степень");
+            Console.WriteLine($"1.1 ^ 1.1 = {Stepen(0.5, 0.5)}"); // 0.707
+            //Console.WriteLine($" 0 ^ 0.1 = {ExponentiationRealNumber(0, 1.1)}"); //Ошибка
+
+            Console.WriteLine("Простое ли число:");
+
+            Console.WriteLine($"1:{PrimeNumber(2)}"); //false
+            Console.WriteLine($"6:{PrimeNumber(100)}"); //false
+            Console.WriteLine($"7:{PrimeNumber(7)}"); //true
+
+            Console.Read();
+        }
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,108 @@
+namespace TestProject
+{
+    public class Tests
+    {
+        [Test]
+        public void Sort_Test1()
+        {
+            int[] array = new int[10] { 1, 7, 10, 24, 65, 12, 56, 5, 1, 2 };
+            int[] array_correct = new int[10] { 1, 1, 2, 5, 7, 10, 12, 24, 56, 65 };
+
+
+            CollectionAssert.AreEqual(Program.Sort(array), array_correct);
+        }
+
+        [Test]
+        public void Sort_Test2()
+        {
+            int[] array = new int[10] { -1, -2, 3, -4, -5, -6, 7, 8, 9, 10 };
+            int[] array_correct = new int[10] { -6, -5, -4, -2, -1, 3, 7, 8, 9, 10 };
+
+            CollectionAssert.AreEqual(Program.Sort(array), array_correct);
+        }
+
+        [Test]
+        public void Test_Palindrome_1()
+        {
+            string str = "a";
+
+            Assert.False(Program.Palindrome(str));
+        }
+        [Test]
+        public void Test_Palindrome_2()
+        {
+            string str = string.Empty;
+
+            Assert.False(Program.Palindrome(str));
+        }
+        [Test]
+        public void Test_Palindrome_3()
+        {
+            string str = "111";
+
+            Assert.True(Program.Palindrome(str));
+        }
+
+        [Test]
+        public void Test_Factorial_1()
+        {
+            Assert.That(Program.Factorial(6), Is.EqualTo(720));
+        }
+        [Test]
+        public void Test_Factorial_2()
+        {
+            Exception ex = Assert.Throws<Exception>(() => Program.Factorial(-10)); // ыавыпавррпвроывфыв
+            Assert.That(ex.Message, Is.EqualTo("����� ������ ����"));
+        }
+
+        [Test]
+        public void Test_Fibonacci_1()
+        {
+            Assert.That(Program.Fibonacci(14), Is.EqualTo(377));
+        }
+
+        [Test]
+        public void Test_Fibonacci_2()
+        {
+            Exception ex = Assert.Throws<Exception>(() => Program.Fibonacci(-1));
+
+            Assert.That(ex.Message, Is.EqualTo("����� ������ ����"));
+        }
+
+        [Test]
+        public void Test_Stepen_1()
+        {
+            Assert.That(Math.Round(Program.Stepen(1.2, 2.5), 4), Is.EqualTo(1.5774));
+        }
+        [Test]
+        public void Test_Stepen_2()
+        {
+            Exception ex = Assert.Throws<Exception>(() => Program.Stepen(-0.551, 0.111));
+
+            Assert.That(ex.Message, Is.EqualTo("����� ������ ��� ��������� ����"));
+        }
+        [Test]
+        public void Test_Stepen_3()
+        {
+            Exception ex = Assert.Throws<Exception>(() => Program.Stepen(0.0, -0.421));
+
+            Assert.That(ex.Message, Is.EqualTo("����� ������ ��� ��������� ����"));
+        }
+
+        [Test]
+        public void Test_PrimeNumber_1()
+        {
+            Assert.True(Program.PrimeNumber(3));
+            Assert.True(Program.PrimeNumber(7));
+            Assert.True(Program.PrimeNumber(11));
+        }
+
+        [Test]
+        public void Test_PrimeNumber_2()
+        {
+            Assert.False(Program.PrimeNumber(4));
+            Assert.False(Program.PrimeNumber(8));
+            Assert.False(Program.PrimeNumber(12));
+        }
+    }
+}
